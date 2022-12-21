1764e42cbe0b590f6e88f1a555c1f356eb88d080
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-21 12:26:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 using namespace std;
 
-double f( double a){
-    double y=0;
+float f( float a){
+    float y=0;
     y= ((a*a)* cos(a))+1;
     return y;
 }
 
 int main() {
-    double a=0, b=0, m=0,err=1;
+    float a=0, b=0, m=0,err=1;
     while((f(a)* f(b))>=0){
         cout<<"inserire estremi"<<endl;
         cin>>a>>b;
