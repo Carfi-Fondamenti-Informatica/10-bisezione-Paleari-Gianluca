3e9bba04ac30e1d28b917d04a05d78b4346a4695
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-21 12:16:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,45 +2,36 @@
 #include <cmath>
 using namespace std;
 
-double f(double y){
-    double a=0
-    a=(y*y)*cos(y)+1;
-    return a;
+double f( double a){
+    double y=0;
+    y= ((a*a)* cos(a))+1;
+    return y;
 }
-int main() {
-    double a=0, b=0,x=0,err=1;
 
-    while ((f(a) * f(b)) >= 0) {
-        cout << "inserire estremi " << endl;
-        cin >> a >> b;
+int main() {
+    double a=0, b=0, x=0,err=1;
+    while((f(a)* f(b))>=0){
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
     }
 
-
-
-    while(f(x)!=0){
-
-         x=(a+b)/2;
-        if(f(x)*f(b)<0)
-        {
+    while (f(x)!=0){
+        x=((a+b)/2);
+        if ((f(x)*f(b))<0){
             a=x;
-
+        }else{
+            b=x;
         }
-        else {
-            b = x;
-        }
-        err=abs((b-a)/2);
-        if(err<(1e-6)) {
+        err= abs((b-a)/2);
+        if (err<(1e-6)){
             break;
         }
-
     }
-    x=x*10000
+
+    x=x*10000;
     x=(int)x;
     x=x/10000;
-    cout<<x;
-
-
-
+    cout<< x;
 
 
     return 0;
