3ae8944353a22a69c2fce2b09e430677fdc8693f
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-21 12:23:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ double f( double a){
 }
 
 int main() {
-    double a=0, b=0, x=0,err=1;
+    double a=0, b=0, m=0,err=1;
     while((f(a)* f(b))>=0){
         cout<<"inserire estremi"<<endl;
         cin>>a>>b;
     }
 
-    while (f(x)!=0){
-        x=((a+b)/2);
-        if ((f(x)*f(b))<0){
-            a=x;
+    while (f(m)!=0){
+        m=((a+b)/2);
+        if ((f(m)*f(b))<0){
+            a=m;
         }else{
-            b=x;
+            b=m;
         }
         err= abs((b-a)/2);
         if (err<(1e-6)){
@@ -28,10 +28,10 @@ int main() {
         }
     }
 
-    x=x*10000;
-    x=(int)x;
-    x=x/10000;
-    cout<< x;
+    m=m*10000;
+    m=(int)m;
+    m=m/10000;
+    cout<< m;
 
 
     return 0;
